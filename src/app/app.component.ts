0a92b2450734577770e5f0713bf9e9d8e3af8dd5
toqa hatem <toqahatem802@gmail.com>
2024-01-16 15:17:30 +0200

    toqa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NavComponent} from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
+import { DetailsComponent } from './details/details.component';
+import { MoviesComponent } from './movies/movies.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { RecommendedComponent } from './recommended/recommended.component';
+import { SignComponent } from './sign/sign.component';
+import { RegisterComponent } from './register/register.component';
+import { WishlistComponent } from './wishlist/wishlist.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,FooterComponent ,NavComponent,HomeComponent ,DetailsComponent , 
+    MoviesComponent,NotfoundComponent ,RecommendedComponent , SignComponent , RegisterComponent , WishlistComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
-export const routes: Routes = [];
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
+import { MoviesComponent } from './movies/movies.component';
+import { DetailsComponent } from './details/details.component';
+import { RecommendedComponent } from './recommended/recommended.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { RegisterComponent } from './register/register.component';
+import { SignComponent } from './sign/sign.component';
+import { WishlistComponent } from './wishlist/wishlist.component';
+import { HomeComponent } from './home/home.component';
+
+export const routes: Routes = [
+    // { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent ,title: 'home',},
+
+    { path: 'nav', component: NavComponent , title: 'nav',} ,
+    { path: 'footer',  component: FooterComponent , title: 'footer',},
+    { path: 'movies',  component: MoviesComponent , title: 'movies',},
+    { path: 'details/:id',  component: DetailsComponent , title: 'details',},
+    { path:'register',  component: RegisterComponent, title: 'register',},
+    { path:'sign',  component: SignComponent , title: 'sign',},
+    { path:'wishlist',  component: WishlistComponent , title: 'wishlist',},
+    { path:'recommended',  component: RecommendedComponent , title: 'recommended',},
+    { path:'**',  component: NotfoundComponent, title: 'notfound',},
+
+];
