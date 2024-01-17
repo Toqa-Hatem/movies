2b630e12d6b38b3b4761108f7da5bfd6a931a054
toqa hatem <toqahatem802@gmail.com>
2024-01-18 01:08:34 +0200

    second

---
+++ src/app/movieslist.service.ts
@@ -0,0 +1,14 @@
+import{ HttpClient} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+
+@Injectable()
+export class MovieslistService {
+
+  constructor( private _httpClient:HttpClient) { }
+  getTrending(): Observable<any> 
+  {
+    return this._httpClient.get('https://api.themoviedb.org/3/movie/popular?api_key=8b77adeec843eca6a643bd9fc7c976f8')
+}
+}
