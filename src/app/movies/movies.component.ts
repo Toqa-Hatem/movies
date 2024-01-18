f4d321b3a4a48cdc3711f2b8dc9d6ff57066650d
Marwan Essam <marwanrabie11@gmail.com>
2024-01-18 04:52:29 +0200

    Interface

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,27 +1,24 @@
-import { Component , OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MovieslistService } from '../movieslist.service';
-import { CommonModule } from '@angular/common'; 
+import { CommonModule } from '@angular/common';
+import { MovieType } from '../interfaces/movie-type';
 // import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-movies',
   standalone: true,
-  imports: [ CommonModule ],
+  imports: [CommonModule],
   templateUrl: './movies.component.html',
-  styleUrl: './movies.component.css'
+  styleUrl: './movies.component.css',
 })
-export class MoviesComponent  implements OnInit{
-  
-  recommendedMovies:any[] = [];
-  imgPrefix:string = 'https://image.tmdb.org/t/p/w500/';
-  constructor(private _MovieslistService: MovieslistService){
-    
-  
-  }
+export class MoviesComponent implements OnInit {
+  recommendedMovies: MovieType[] = [];
+  imgPrefix: string = 'https://image.tmdb.org/t/p/w500/';
+  constructor(private _MovieslistService: MovieslistService) {}
   ngOnInit(): void {
-   this. _MovieslistService.getTrending().subscribe((data=>{
+    this._MovieslistService.getTrending().subscribe((data) => {
       this.recommendedMovies = data.results;
-      console.log(data)   ;       }))
-
-  
-}}
+      console.log(data);
+    });
+  }
+}
