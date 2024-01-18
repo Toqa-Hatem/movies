8a68c7fd0de4f5d55edc5c7b1411b043f9773c0d
Marwan Essam <marwanrabie11@gmail.com>
2024-01-18 22:56:54 +0200

    movie details fixed

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { MovieslistService } from '../movieslist.service';
+import test from 'node:test';
 @Component({
   selector: 'app-details',
   standalone: true,
@@ -12,14 +13,23 @@ export class DetailsComponent {
   movie: any;
   id!: number;
   imgBase: string = 'https://image.tmdb.org/t/p/w500/';
+  companyLogo!: any[];
+  companyLogoArr!: any[];
+
   constructor(
     private activatedRoute: ActivatedRoute,
     private _moviesListService: MovieslistService
   ) {}
   ngOnInit() {
     this.id = this.activatedRoute.snapshot.params['id'];
-    this._moviesListService
-      .getMovieDetails(this.id)
-      .subscribe((res) => (this.movie = res));
+    this._moviesListService.getMovieDetails(this.id).subscribe((res) => {
+      this.movie = res;
+      this.companyLogoArr = this.movie.production_companies.filter(
+        (company: any) => company.logo_path != null
+      );
+      if (this.companyLogoArr.length) {
+        this.companyLogo = this.companyLogoArr[0].logo_path;
+      }
+    });
   }
 }
